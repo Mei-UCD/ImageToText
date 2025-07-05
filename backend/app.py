698598f4e851e3cbafdc145cfaa0dd74f5dd0b44
Mei <102959995+Mei-UCD@users.noreply.github.com>
2025-07-05 18:01:11 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -112,4 +112,5 @@ def ocr():
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
